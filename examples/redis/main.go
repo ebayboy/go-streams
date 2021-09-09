054a8a269b414ffded599902c785b3ab3d482989
fanpengfei1 <fanpengfei@jd.com>
2021-09-09 14:49:47 +0800

    update demo with redis-pubsub.

--- examples/redis/main.go
+++ examples/redis/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"strings"
 	"time"
 
@@ -14,32 +15,42 @@ import (
 
 //docker exec -it pubsub bash
 //https://redis.io/topics/pubsub
-func main() {
-	ctx, cancelFunc := context.WithCancel(context.Background())
 
-	timer := time.NewTimer(time.Minute)
-	go func() {
-		select {
-		case <-timer.C:
-			cancelFunc()
-		}
-	}()
+var toUpper = func(in interface{}) interface{} {
+	msg := in.(*redis.Message)
+	fmt.Println("msg:", msg)
+	return strings.ToUpper(msg.Payload)
+}
 
+//Process:  sub chanin -> ToUpper -> pub chanout
+func testSubpub(ctx *context.Context) {
 	config := &redis.Options{
 		Addr:     "localhost:6379", // use default Addr
-		Password: "",               // no password set
+		Password: "123456",         // no password set
 		DB:       0,                // use default DB
 	}
 
-	source, err := ext.NewRedisSource(ctx, config, "test")
+	source, err := ext.NewRedisSource((*ctx), config, "chanin")
+
 	util.Check(err)
 	flow1 := flow.NewMap(toUpper, 1)
-	sink := ext.NewRedisSink(config, "test2")
+	sink := ext.NewRedisSink(config, "chanout")
 
 	source.Via(flow1).To(sink)
 }
 
-var toUpper = func(in interface{}) interface{} {
-	msg := in.(*redis.Message)
-	return strings.ToUpper(msg.Payload)
+func main() {
+	ctx, cancelFunc := context.WithCancel(context.Background())
+
+	//after 5 second, exit process
+	timer := time.NewTimer(time.Second * 5)
+	go func() {
+		select {
+		case <-timer.C:
+			fmt.Println("cancelFunc...")
+			cancelFunc()
+		}
+	}()
+
+	testSubpub(&ctx)
 }
